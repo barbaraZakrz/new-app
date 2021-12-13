d8e40adab3af151a3926922997513243d0cbf2f6
Basia <barbara.zakrz@gmail.com>
2021-12-13 12:20:53 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import  Header  from './components/header';
+import Footer from './components/footer';
+
+function OurText(){
+  return <p>This is our text</p>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <Header info ="this is My message" myNumber="6" />
+    <Header info ="this is My message" myNumber="4" />
+    <p>Main content</p>
+    <Footer trademark="page by me"/>
     </div>
   );
 }
